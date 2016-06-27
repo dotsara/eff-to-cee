b1ca0e0b14b2cd143a181f919a83bb50cee312bd
Sara Flemming <dotsara@gmail.com>
2016-06-27 14:54:01 -0700

    v2: still converting to both scales, changed results display
    This time around after converting the entered number to both scales,
    the display is "[entered-value]°F is [result-value]°C" and vice
    versa. This is a bit more useful than the previous version.
    
    UP NEXT:
    * a little color and life

--- javascript/conversion.js
+++ javascript/conversion.js
@@ -3,7 +3,6 @@
 var formattedFahr = "&deg;F";
 var formattedCels = "&deg;C";
 
-
 $(document).ready(function() {
   console.log("hello! we are ready to convert.");
 
@@ -18,29 +17,28 @@ $(document).ready(function() {
     console.log("tempToConvert: " + tempToConvert);
 
     /*
-      On this first pass, I'm not doing anything fancier than checking
-      that the input was a number and then converting the number to F
-      and C and displaying that result.
+      this time, I convert the number entered (still checking that it's
+      a number) and display the number as a temp in both scales and 
+      converted in both scales.
     */
     if ($.isNumeric(tempToConvert)) {
       convertedFahr = fahrToCels(tempToConvert);
       convertedCels = celsToFahr(tempToConvert);
 
-      console.log("convertedFahr: " + convertedFahr);
-      console.log("convertedCels: " + convertedCels);
-      console.log("Original number converted!");
-
-      var resultFahr = formatConvertedTemp(tempToConvert, convertedFahr, formattedFahr);
-      var resultCels = formatConvertedTemp(tempToConvert, convertedCels, formattedCels);
+      var resultFahr = "<p>" + tempToConvert + formattedFahr + " is " + convertedCels +  formattedCels + "</p>";
+      var resultCels = "<p>" + tempToConvert + formattedCels + " is " + convertedFahr + formattedFahr + "</p>";
       
-      console.log(resultFahr);
-      console.log(resultCels);
-      
+      /*
+        emptying the results, in case someone's entering new numbers
+        without reloading the page
+      */
+      $resultsContainer.empty();
       $resultsContainer.append(resultFahr, resultCels);
     } else {
       $resultsContainer.addClass("error");
       $resultsContainer.html("Ah, we can only convert numbers.");
-    }
+    };
+
   })
 });
 
@@ -50,8 +48,4 @@ var fahrToCels = function (fahr) {
 
 var celsToFahr = function (celsius) {
   return (((celsius * 9) / 5) + 32).toFixed(0);
-};
-
-var formatConvertedTemp = function(orig, temp, scale) {
-  return "<p>" + orig + " is " + temp + scale + "!</p>";
 };
